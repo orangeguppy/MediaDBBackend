36139a1b1117ea7f87005a94ccab9341427aa2d5
Nicole Oo <vuwet467@gmail.com>
2023-08-03 23:57:11 +0800

    Driver connectivity test

---
+++ journalist.py
@@ -0,0 +1,9 @@
+# Driver import
+from neo4j import GraphDatabase
+
+# URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
+URI = "neo4j+s://55770697.databases.neo4j.io"
+AUTH = ("neo4j", "D8BcIOw4QIFj-v7E1QgRAm71kcQ0cLlDx_u3pB4YauQ")
+
+with GraphDatabase.driver(URI, auth=AUTH) as driver: 
+    driver.verify_connectivity() 
