6ffcd21c2ab478b99027d355f32da15e26c8c17e
Nicole <vuwet467@gmail.com>
2023-08-04 18:40:42 +0800

    First commit

--- journalist.py
+++ journalist.py
@@ -7,3 +7,10 @@ AUTH = ("neo4j", "D8BcIOw4QIFj-v7E1QgRAm71kcQ0cLlDx_u3pB4YauQ")
 
 with GraphDatabase.driver(URI, auth=AUTH) as driver: 
     driver.verify_connectivity() 
+
+# api/journalists GET Fetch all journalists based on specified tags
+def find_all_journalists():
+    match (person:journalist)
+    return person.title
+
+
---
+++ medialist.py
@@ -0,0 +1,7 @@
+# api/medialists GET Fetch all media lists
+
+# api/medialists POST Add a new media list to the database
+
+# api/medialists/{id} GET Fetch a media list based on ID
+
+# api/medialists/{id} POST Add a new journalist to the media list
